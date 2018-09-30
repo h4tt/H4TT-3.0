a83f492362a80e5f2eef25c886a03d45970814d8
Dave <davidpetrasovic@gmail.com>
2018-09-29 22:50:56 -0400

    Added a web challenge involving user agent spoofing (#7)

---
+++ web/not_the_bots_you_are_looking_for/challenge/index.js
@@ -0,0 +1,29 @@
+// config
+const HOST_PORT = 3000;
+const FLAG = 'flag{r0b0ts_d0t_txt_1s_n0t_s3cur1ty}';
+
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
+app.listen(HOST_PORT);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+'/home.html');
+});
+
+app.get('/robots.txt', (req, res) => {
+  const userAgent = req.headers['user-agent'];
+  if(userAgent.indexOf('Googlebot')>-1){
+    res.sendFile(__dirname+'/robots.txt');
+  }else{
+    res.sendStatus(401);
+  }
+});
+
+app.get('/flag-e344db6c4d829be348157715b2658ce5d18a659f', (req, res) => {
+  res.send(`${FLAG} <p> You should not rely on obfuscating robtos.txt for security.`);
+});
+
+app.get('/*', (req, res) => {
+    res.sendStatus(404);
+});
