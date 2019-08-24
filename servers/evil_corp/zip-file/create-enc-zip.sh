b627a618656946d8f8830e6f3b46f193c8abed73
Francisco Trindade <francisco.ad.trindade@gmail.com>
2019-08-24 15:29:07 -0400

    [evil-corp-challenge-docker] removed unsued shell script

--- servers/evil_corp/zip-file/create-enc-zip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-zipname=${1}
-password=$(cat ./password.txt)
-
-echo ${password}
-
-echo "Rebuilding the encrypted zip file..."
-zip -e -j --password "$(cat password.txt)" ${zipname} flag.txt top-secret.md
