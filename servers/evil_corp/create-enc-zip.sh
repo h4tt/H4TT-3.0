c32cb2d94b30d3b77854717e4237a756a3c6e60b
Francisco Trindade <francisco.ad.trindade@gmail.com>
2019-08-24 15:24:12 -0400

    [evil-corp-challenge-docker] Cleaned up docker file and fixed build and run script

--- servers/evil_corp/create-enc-zip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-zipname=TOP-SECRET.zip
-
-echo "Rebuilding the encrypted zip file..."
-rm app/public/${zipname}
-zip -e -j --password "$(cat zip-file/password.txt)" ${zipname} zip-file/flag.txt zip-file/top-secret.md
-mv ${zipname} app/public/
