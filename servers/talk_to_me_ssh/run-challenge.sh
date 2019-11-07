6b33f9dde9a21e0bdb78d166a94f7c081104b4fe
Forest <wizcardforest@gmail.com>
2019-11-07 17:09:35 -0500

    Holy fuck it works

--- servers/talk_to_me_ssh/run-challenge.sh
+++ servers/talk_to_me_ssh/run-challenge.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run -it --rm --user eviluser servers_talk-to-me-challenge
+export DOCKER_HOST=tcp://docker:2376
+export DOCKER_TLS_VERIFY=1
+export DOCKER_CERT_PATH=/certs/client
+export DOCKER_TLS_CERTDIR=/certs
+docker run -it --rm --user eviluser talk-to-me-challenge
